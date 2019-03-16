fc4dd9e4e482cb58297f0f1e2ad74d61ce47f824
Gianos92 <fiorillagianmarco@gmail.com>
2019-03-16 11:16:06 +0100

    pre-MVP of the Running Tracker up.
    Main features missing:
    - summary views of the workout
    - list view formatting

--- app/src/main/java/com/self/runningtracker/AddWorkoutActivity.java
+++ app/src/main/java/com/self/runningtracker/AddWorkoutActivity.java
@@ -4,18 +4,15 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,8 +23,6 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.self.runningtracker.data.WorkoutContract.WorkoutEntry;
-import com.self.runningtracker.data.WorkoutDbHelper;
-import com.self.runningtracker.data.WorkoutProvider;
 
 public class AddWorkoutActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
