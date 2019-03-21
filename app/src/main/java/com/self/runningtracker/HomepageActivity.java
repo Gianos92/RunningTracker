35b2dff14a26d9e3714b2fa5efa0012c27e0d805
Gianos92 <fiorillagianmarco@gmail.com>
2019-03-21 01:38:10 +0100

    pre-MVP of the Running Tracker up.
    Main features missing:
    - list view formatting
    - General reviewing

--- app/src/main/java/com/self/runningtracker/HomepageActivity.java
+++ app/src/main/java/com/self/runningtracker/HomepageActivity.java
@@ -112,6 +112,8 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
         // Stores the query result to the selected TextView.
         dateTextView.setText(Integer.toString(data.getCount()));
 
+        //----------------------------Code to populate the total distance TextView-----------------//
+
         // Iterates through each workout distance to get the total distance.
         if (data.moveToFirst()) {
             float distance = 0;
@@ -122,24 +124,110 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
                     distance += workoutDistance;
                 }
             } finally {
+                // Includes the first row of the Cursor.
                 data.moveToFirst();
                 float firstWorkout = data.getFloat(data.getColumnIndex(WorkoutEntry.COLUMN_DISTANCE));
                 distance += firstWorkout;
+
+                // Finds the view that will be used to hold the query result.
+                TextView distanceTextView = findViewById(R.id.distance_overview_kpi);
+
+                // Stores the calculation result to the selected TextView.
+                distanceTextView.setText(Float.toString(distance));
             }
-
-            // Finds the view that will be used to hold the query result.
-            TextView distanceTextView = findViewById(R.id.distance_overview_kpi);
-
-            // Stores the query result to the selected TextView.
-            distanceTextView.setText(Float.toString(distance));
 
         }else {
             // Finds the view that will be used to hold the query result.
             TextView distanceTextView = findViewById(R.id.distance_overview_kpi);
 
-            // Stores the query result to the selected TextView.
+            // Stores the calculation result to the selected TextView.
             distanceTextView.setText("00.00");
         }
+
+        //----------------------------Code to populate the total duration TextView-----------------//
+
+        // Iterates through each workout duration to get the total duration.
+        if (data.moveToFirst()) {
+            int durationInSecs = 0;
+            data.moveToFirst();
+            try {
+                while (data.moveToNext()) {
+                    String duration = data.getString(data.getColumnIndex(WorkoutEntry.COLUMN_DURATION));
+
+                    // Calculates the total number of seconds in the current Cursor row.
+                    String[] units = duration.split(":"); //will break the string up into an array
+                    int hours = Integer.parseInt(units[0]); //first element
+                    int minutes = Integer.parseInt(units[1]); //second element
+                    int seconds = Integer.parseInt(units[2]); //third element
+                    int workoutDuration = 3600* hours + 60*minutes + seconds; //adds up our values
+
+                    durationInSecs += workoutDuration;
+                }
+            } finally {
+                // Includes the first row of the Cursor.
+                data.moveToFirst();
+                String firstWorkout = data.getString(data.getColumnIndex(WorkoutEntry.COLUMN_DURATION));
+
+                // Calculates the total number of seconds in the first row in the Cursor.
+                String[] units1 = firstWorkout.split(":"); //will break the string up into an array
+                int hours1 = Integer.parseInt(units1[0]); //first element
+                int minutes1 = Integer.parseInt(units1[1]); //second element
+                int seconds1 = Integer.parseInt(units1[2]); //third element
+                int FirstWorkoutDuration = 3600* hours1 + 60*minutes1 + seconds1; //adds up our values
+
+                // Gets the total number of seconds run.
+                durationInSecs += FirstWorkoutDuration;
+
+                /**
+                 * Gets the hours.
+                 */
+                int numOfHoursInt = durationInSecs/3600;
+
+                /**
+                 * Gets the minutes.
+                 */
+                // Formats the integer so that it shows the decimals.
+                float numOfHoursFloat = (float)durationInSecs/3600;
+
+                // Isolates the decimal numbers.
+                float DecimalOfMinutes = numOfHoursFloat-numOfHoursInt;
+
+                // Gets the number of minutes.
+                float numOfMinutesFloat = DecimalOfMinutes*60;
+
+                // Gets the integer of minutes so that it always shows two digits.
+                int numOfMinutesInt = (int) numOfMinutesFloat;
+
+                /**
+                 * Gets the minutes.
+                 */
+                // Isolates the decimal numbers.
+                float DecimalOfSeconds = numOfMinutesFloat - numOfMinutesInt;
+
+                // Gets the number of seconds.
+                float numOfSecondsFloat = DecimalOfSeconds*60;
+
+                // Gets the integer of seconds so that it always shows two digits.
+                int numOfSecondsInt = (int) numOfSecondsFloat;
+
+                // Formats the hours so that they always shows two digits and stores them in the array.
+                String workoutDuration = Integer.toString(numOfHoursInt) + ":" + String.format("%02d", numOfMinutesInt) + ":" + String.format("%02d",numOfSecondsInt);
+
+                // Finds the view that will be used to hold the query result.
+                TextView durationTextView = findViewById(R.id.duration_overview_kpi);
+
+                // Stores the calculation result to the selected TextView.
+                durationTextView.setText(workoutDuration);
+            }
+
+        }else {
+            // Finds the view that will be used to hold the calculation result.
+            TextView distanceTextView = findViewById(R.id.duration_overview_kpi);
+
+            // Stores the calculation result to the selected TextView.
+            distanceTextView.setText("00:00:00");
+        }
+
     }
 
     @Override
